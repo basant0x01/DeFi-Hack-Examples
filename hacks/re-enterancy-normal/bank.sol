00ed8c27cd57f1aa667d9acb3b044490a0cd3bcf
Basant Karki <123530150+basant0x01@users.noreply.github.com>
2023-06-11 16:48:52 +0545

    Update bank.sol

--- hacks/re-enterancy-normal/bank.sol
+++ hacks/re-enterancy-normal/bank.sol
@@ -4,13 +4,16 @@ pragma solidity ^0.8.7;
 // This is the simple simulation of DAO RE-ENTERANCY ATTACK
 
 contract globalIMEBank{
-
+   
+   // mapping for checking balances using account address.
    mapping(address => uint) public balances;
-
+   
+   // deposite function will deposit balance to the contract from account
    function deposit() external payable {
       balances[msg.sender] += msg.value;
    }
-
+   
+   // withdraw function will withdraw all the deposited funds and set the account owner balance as 0;
    function withdraw() external payable {
       uint bal = balances[msg.sender];
       (bool success,) = msg.sender.call{value: bal}("");
